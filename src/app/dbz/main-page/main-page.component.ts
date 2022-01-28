8de965932398a351dc3c41d5144dc9ceadb0fb95
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2022-01-28 20:26:21 +0100

    Uso de array de Personajes y añadir un nuevo personaje

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,18 +12,44 @@ interface Personaje {
 })
 export class MainPageComponent implements OnInit {
 
+  personajes: Personaje[] = [
+    {
+      nombre: "Krillin",
+      poder: 700
+    },{
+      nombre: "Goku",
+      poder: 15000
+    },{
+      nombre: "Vegeta",
+      poder: 8500
+    }
+  ]
+
   nuevo: Personaje = {
-    nombre: 'Trucks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   agregar() {
-    console.log("Has presionado el botón Agregado");
-    console.log( this.nuevo );
+    if( this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+
+    //Añadir personaje
+    this.personajes.push(this.nuevo);
+    console.log('Personaje añadido: ', this.nuevo );
+
+    //Limpiar nuevo
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
   }
 
 }
