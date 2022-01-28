2c981dec76430b3bb85527c3b920f53749d60148
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2022-01-28 19:28:48 +0100

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,6 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000
+  }
   constructor() { }
 
   ngOnInit(): void {
@@ -14,6 +23,7 @@ export class MainPageComponent implements OnInit {
 
   agregar() {
     console.log("Has presionado el botón Agregado");
+    console.log( this.nuevo );
   }
 
 }
