142406734652220bfc853fc1013e26dd2c52b2dd
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2022-02-01 09:59:27 +0100

    Crear componente Hijo

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styles: [
+  ]
+})
+export class PersonajesComponent {
+
+  personajes: any[] = [];
+
+}
